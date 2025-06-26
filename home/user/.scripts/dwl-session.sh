7039c23d867630469916639513401fa4f0768852
mmistika <mistikasoft@gmail.com>
2025-06-26 23:56:23 +0200

    clear scripts/dwl-session.sh

--- home/user/.scripts/dwl-session.sh
+++ home/user/.scripts/dwl-session.sh
@@ -2,5 +2,3 @@
 
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP
-# systemctl --user start xdg-desktop-portal-wlr.service
-# exec dbus-run-session -- sh -c "while true; do sleep 3600; done"
