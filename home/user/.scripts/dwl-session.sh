4ae24fea53a9e190314b1fb4fcae696be24ab2ba
mmistika <mistikasoft@gmail.com>
2025-07-10 14:44:29 +0200

    add wallpaper

--- home/user/.scripts/dwl-session.sh
+++ home/user/.scripts/dwl-session.sh
@@ -3,4 +3,5 @@
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP
 
+swaybg -i ~/.scripts/wallpaper.jpg &
 swayidle timeout 10 "makoctl mode -s idle" resume "makoctl mode -s default" &
