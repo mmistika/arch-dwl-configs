b645cdcb42b996d7f588973957497cad88495030
mmistika <mistikasoft@gmail.com>
2025-07-10 14:35:24 +0200

    add mako config

--- home/user/.scripts/dwl-session.sh
+++ home/user/.scripts/dwl-session.sh
@@ -2,3 +2,5 @@
 
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP
 systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP
+
+swayidle timeout 10 "makoctl mode -s idle" resume "makoctl mode -s default" &
