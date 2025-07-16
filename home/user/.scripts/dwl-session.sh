928416ca00e78d6da17fd1855bbfa258c87c3d4c
mmistika <mistikasoft@gmail.com>
2025-07-16 21:56:23 +0200

    fix screencast

--- home/user/.scripts/dwl-session.sh
+++ home/user/.scripts/dwl-session.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP
-systemctl --user import-environment WAYLAND_DISPLAY XDG_CURRENT_DESKTOP XDG_SESSION_DESKTOP
-
 swaybg -i ~/.scripts/wallpaper.jpg &
 swayidle timeout 10 "makoctl mode -s idle" resume "makoctl mode -s default" &
